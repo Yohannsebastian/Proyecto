dbf5ad386cdf0d84009f4fec6af4c6a28d5c8362
Yohannsebastian <yohannsebastian36@gmail.com>
2024-04-05 19:39:21 -0500

    Primero

---
+++ Proyecto/project/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class contactanos(models.Model):
+    nombre = models.CharField(max_length = 50)
+    apellido = models.CharField(max_length = 50)
+    celular = models.CharField(max_length = 10)
+    email = models.EmailField()
